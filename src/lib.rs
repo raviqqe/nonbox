fbcdb531203f8eedd9a23aa1f4f54f0160a09310
Yota Toyama <raviqqe@gmail.com>
2024-08-12 14:45:35 +0900

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,21 @@ pub const EXPONENT_MASK_OFFSET: usize = 48;
 
 /// An exponent mask.
 pub const EXPONENT_MASK: u64 = 0x7ff0 << EXPONENT_MASK_OFFSET;
+
+pub const fn r#box(value: u64) -> f64 {
+    f64::NAN.to_bits() | value
+}
+
+pub const fn unbox(value: u64) -> f64 {
+    f64::NAN.to_bits() | value
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn unbox_value() {
+        assert_eq!();
+    }
+}
