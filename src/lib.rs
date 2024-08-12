cfba508e3a853db5c82e623acc8816b5d56d3fa0
Yota Toyama <raviqqe@gmail.com>
2024-08-12 14:41:51 +0900

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![no_std]
 
+/// An offset to an exponent mask.
 pub const EXPONENT_MASK_OFFSET: usize = 48;
+/// An exponent mask.
 pub const EXPONENT_MASK: u64 = 0x7ff0 << EXPONENT_MASK_OFFSET;
-
-const fn build_mask(sub_mask: usize) -> u64 {
-    ((sub_mask as u64) << TYPE_MASK_OFFSET) | EXPONENT_MASK
-}
