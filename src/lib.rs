957ab9509798133b5fafb575727e6e676b835fab
Yota Toyama <raviqqe@gmail.com>
2024-08-12 16:22:33 +1000

    Implement NaN boxing (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,59 @@
 #![doc = include_str!("../README.md")]
+#![no_std]
+
+#[cfg(test)]
+extern crate std;
+
+/// An offset to an exponent mask.
+pub const EXPONENT_MASK_OFFSET: usize = 48;
+
+/// An exponent mask.
+pub const EXPONENT_MASK: u64 = 0x7ff8 << EXPONENT_MASK_OFFSET;
+
+/// A boxed value mask.
+pub const BOXED_VALUE_MASK: u64 = !(0xfff8 << EXPONENT_MASK_OFFSET);
+
+/// Boxes a value.
+///
+/// The `value` needs to be less than `1 << 52`. Otherwise, it is truncated.
+pub fn r#box(value: u64) -> f64 {
+    f64::from_bits(EXPONENT_MASK | value)
+}
+
+/// Unboxes a value.
+pub fn unbox(number: f64) -> Option<u64> {
+    number
+        .is_nan()
+        .then_some(number.to_bits() & BOXED_VALUE_MASK)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_mask() {
+        assert_eq!(EXPONENT_MASK, f64::NAN.to_bits());
+    }
+
+    #[test]
+    fn box_value() {
+        assert!(r#box(42).is_nan());
+    }
+
+    #[test]
+    fn unbox_value() {
+        assert_eq!(unbox(r#box(0)), Some(0));
+        assert_eq!(unbox(r#box(1)), Some(1));
+        assert_eq!(unbox(r#box(7)), Some(7));
+        assert_eq!(unbox(r#box(42)), Some(42));
+    }
+
+    #[test]
+    fn unbox_f64_value() {
+        assert_eq!(unbox(0.0), None);
+        assert_eq!(unbox(-1.0), None);
+        assert_eq!(unbox(1.0), None);
+        assert_eq!(unbox(42.0), None);
+    }
+}
