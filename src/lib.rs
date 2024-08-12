d5679179eff69be426f7c96132cb47ceb8fe5597
Yota Toyama <raviqqe@gmail.com>
2024-08-12 14:41:19 +0900

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 #![doc = include_str!("../README.md")]
+#![no_std]
+
+pub const EXPONENT_MASK_OFFSET: usize = 48;
+pub const EXPONENT_MASK: u64 = 0x7ff0 << EXPONENT_MASK_OFFSET;
+
+const fn build_mask(sub_mask: usize) -> u64 {
+    ((sub_mask as u64) << TYPE_MASK_OFFSET) | EXPONENT_MASK
+}
