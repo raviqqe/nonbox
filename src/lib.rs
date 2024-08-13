536f74392b134ef9888e8c908dac8907d52f2601
Yota Toyama <raviqqe@gmail.com>
2024-08-13 21:20:03 +0900

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,12 @@ const BOXED_VALUE_MASK: u64 = !(0xfff8 << EXPONENT_MASK_OFFSET);
 /// Boxes an unsigned value into `f64`.
 ///
 /// The `value` needs to be less than `1 << 51`. Otherwise, it is truncated.
-pub fn r#box(value: u64) -> f64 {
+pub fn box_unsigned(value: u64) -> f64 {
     f64::from_bits(EXPONENT_MASK | value)
 }
 
 /// Unboxes an unsigned value from `f64`.
-pub fn unbox(number: f64) -> Option<u64> {
+pub fn unbox_unsigned(number: f64) -> Option<u64> {
     number
         .is_nan()
         .then_some(number.to_bits() & BOXED_VALUE_MASK)
@@ -30,7 +30,7 @@ pub fn box_signed(value: i64) -> f64 {
 
 /// Unboxes a signed value from `f64`.
 pub fn unbox_signed(number: f64) -> Option<i64> {
-    unbox(number).map(|value| {
+    unbox_unsigned(number).map(|value| {
         (if number.to_bits() & SIGN_MASK == 0 {
             1
         } else {
@@ -41,12 +41,12 @@ pub fn unbox_signed(number: f64) -> Option<i64> {
 
 /// Boxes a value into `u64` representation of `f64`.
 pub fn box_u64(value: u64) -> u64 {
-    r#box(value).to_bits()
+    box_unsigned(value).to_bits()
 }
 
 /// Unboxes a value from `u64` representation of `f64`.
 pub fn unbox_u64(number: u64) -> Option<u64> {
-    unbox(f64::from_bits(number))
+    unbox_unsigned(f64::from_bits(number))
 }
 
 #[cfg(test)]
@@ -60,18 +60,18 @@ mod tests {
 
     #[test]
     fn box_unsigned_value() {
-        assert!(r#box(0).is_nan());
-        assert!(r#box(1).is_nan());
-        assert!(r#box(7).is_nan());
-        assert!(r#box(42).is_nan());
+        assert!(box_unsigned(0).is_nan());
+        assert!(box_unsigned(1).is_nan());
+        assert!(box_unsigned(7).is_nan());
+        assert!(box_unsigned(42).is_nan());
     }
 
     #[test]
     fn unbox_unsigned_value() {
-        assert_eq!(unbox(r#box(0)), Some(0));
-        assert_eq!(unbox(r#box(1)), Some(1));
-        assert_eq!(unbox(r#box(7)), Some(7));
-        assert_eq!(unbox(r#box(42)), Some(42));
+        assert_eq!(unbox_unsigned(box_unsigned(0)), Some(0));
+        assert_eq!(unbox_unsigned(box_unsigned(1)), Some(1));
+        assert_eq!(unbox_unsigned(box_unsigned(7)), Some(7));
+        assert_eq!(unbox_unsigned(box_unsigned(42)), Some(42));
     }
 
     #[test]
@@ -99,10 +99,10 @@ mod tests {
 
     #[test]
     fn unbox_f64_value() {
-        assert_eq!(unbox(0.0), None);
-        assert_eq!(unbox(-1.0), None);
-        assert_eq!(unbox(1.0), None);
-        assert_eq!(unbox(42.0), None);
+        assert_eq!(unbox_unsigned(0.0), None);
+        assert_eq!(unbox_unsigned(-1.0), None);
+        assert_eq!(unbox_unsigned(1.0), None);
+        assert_eq!(unbox_unsigned(42.0), None);
     }
 
     #[test]
