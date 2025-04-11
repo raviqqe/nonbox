3771e6347505603e42950a955b2efb608c5dd2bc
Yota Toyama <raviqqe@gmail.com>
2025-04-12 08:21:18 +0900

    Sum benchmark (#27)

---
+++ benches/main.rs
@@ -0,0 +1,36 @@
+#![allow(missing_docs)]
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use nonbox::f64;
+
+const ITERATION_COUNT: usize = 10000;
+
+fn sum(criterion: &mut Criterion) {
+    criterion.bench_function("sum_pure", |bencher| {
+        bencher.iter(|| {
+            let mut sum = 0.0f64;
+
+            for index in 0..black_box(ITERATION_COUNT as u64) {
+                sum += index as f64;
+            }
+
+            black_box(sum);
+        })
+    });
+
+    criterion.bench_function("sum", |bencher| {
+        bencher.iter(|| {
+            let mut sum = 0.0f64;
+
+            for index in 0..black_box(ITERATION_COUNT as u64) {
+                sum += f64::box_unsigned(index);
+            }
+
+            black_box(sum);
+        })
+    });
+}
+
+criterion_group!(benches, sum);
+
+criterion_main!(benches);
