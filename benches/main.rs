405d42bdc27b0fd349e2d5a72799ce2206bc833e
Yota Toyama <raviqqe@gmail.com>
2025-04-12 15:27:48 +0900

    More benchmarks (#35)

--- benches/main.rs
+++ benches/main.rs
@@ -23,7 +23,7 @@ fn box_unsigned(criterion: &mut Criterion) {
     criterion.bench_function("box_unsigned", |bencher| {
         bencher.iter(|| {
             for index in 0..ITERATION_COUNT as u64 {
-                black_box(f64::box_unsigned(index));
+                black_box(f64::box_unsigned(black_box(index)));
             }
         })
     });
@@ -33,12 +33,39 @@ fn box_signed(criterion: &mut Criterion) {
     criterion.bench_function("box_signed", |bencher| {
         bencher.iter(|| {
             for index in 0..ITERATION_COUNT as i64 {
-                black_box(f64::box_signed(index));
+                black_box(f64::box_signed(black_box(index)));
             }
         })
     });
 }
 
-criterion_group!(benches, sum, box_unsigned, box_signed);
+fn unbox_unsigned(criterion: &mut Criterion) {
+    criterion.bench_function("unbox_unsigned", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f64::unbox_unsigned(black_box(index)));
+            }
+        })
+    });
+}
+
+fn unbox_signed(criterion: &mut Criterion) {
+    criterion.bench_function("unbox_signed", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f64::unbox_signed(black_box(index)));
+            }
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    sum,
+    box_unsigned,
+    box_signed,
+    unbox_unsigned,
+    unbox_signed
+);
 
 criterion_main!(benches);
