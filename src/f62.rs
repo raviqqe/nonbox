bd0d1e5f82ae536a1cc6daaee5e2f54461429818
Yota Toyama <raviqqe@gmail.com>
2025-04-12 11:21:57 -0700

    Fix

--- src/f62.rs
+++ src/f62.rs
@@ -1,14 +1,14 @@
 //! NaN boxing for 62-bit floating-pointer numbers encompassing 63-bit integers
 //! and 62-bit payloads.
 
-const EXPONENT_MASK_OFFSET: usize = 48;
-const SIGN_MASK: u64 = 1 << 63;
-const EXPONENT_MASK: u64 = 0x7ffc << EXPONENT_MASK_OFFSET;
-const PAYLOAD_MASK: u64 = !(0xfffc << EXPONENT_MASK_OFFSET);
+/// Boxes a 50-bit unsigned integer.
+pub const fn box_integer(i64: i64) -> u64 {
+    (integer << 1) as _
+}
 
 /// Boxes a 50-bit unsigned integer.
-pub const fn box_unsigned(payload: u64) -> u64 {
-    EXPONENT_MASK | payload
+pub const fn unbox_integer(payload: i64) -> u64 {
+    integer << 1
 }
 
 /// Unboxes a 50-bit unsigned integer.
@@ -18,9 +18,4 @@ pub const fn unbox_unsigned(number: u64) -> Option<u64> {
     } else {
         None
     }
-}
-
-/// Returns `true` if a payload is boxed in a given number.
-pub const fn is_boxed(number: u64) -> bool {
-    number & EXPONENT_MASK == EXPONENT_MASK
 }
