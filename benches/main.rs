1d2c0f89ee2048733419f36d1bed4d06ce87f792
Yota Toyama <raviqqe@gmail.com>
2025-04-12 14:11:31 -0700

    Fix

--- benches/main.rs
+++ benches/main.rs
@@ -1,7 +1,7 @@
 #![allow(missing_docs)]
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use nonbox::f64;
+use nonbox::{f62, f64};
 
 const ITERATION_COUNT: usize = 10000;
 
@@ -19,38 +19,32 @@ fn sum(criterion: &mut Criterion) {
     });
 }
 
-fn box_unsigned(criterion: &mut Criterion) {
-    criterion.bench_function("box_unsigned", |bencher| {
+fn f64(criterion: &mut Criterion) {
+    criterion.bench_function("f64_box_unsigned", |bencher| {
         bencher.iter(|| {
             for index in 0..ITERATION_COUNT as u64 {
                 black_box(f64::box_unsigned(black_box(index)));
             }
         })
     });
-}
 
-fn box_signed(criterion: &mut Criterion) {
-    criterion.bench_function("box_signed", |bencher| {
+    criterion.bench_function("f64_box_signed", |bencher| {
         bencher.iter(|| {
             for index in 0..ITERATION_COUNT as i64 {
                 black_box(f64::box_signed(black_box(index)));
             }
         })
     });
-}
 
-fn unbox_unsigned(criterion: &mut Criterion) {
-    criterion.bench_function("unbox_unsigned", |bencher| {
+    criterion.bench_function("f64_unbox_unsigned", |bencher| {
         bencher.iter(|| {
             for index in 0..ITERATION_COUNT as u64 {
                 black_box(f64::unbox_unsigned(black_box(index)));
             }
         })
     });
-}
 
-fn unbox_signed(criterion: &mut Criterion) {
-    criterion.bench_function("unbox_signed", |bencher| {
+    criterion.bench_function("f64_unbox_signed", |bencher| {
         bencher.iter(|| {
             for index in 0..ITERATION_COUNT as u64 {
                 black_box(f64::unbox_signed(black_box(index)));
@@ -59,13 +53,56 @@ fn unbox_signed(criterion: &mut Criterion) {
     });
 }
 
-criterion_group!(
-    benches,
-    sum,
-    box_unsigned,
-    box_signed,
-    unbox_unsigned,
-    unbox_signed
-);
+fn f62(criterion: &mut Criterion) {
+    criterion.bench_function("f62_box_payload", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::box_payload(black_box(index)));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_unbox_payload", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::unbox_payload(black_box(index)));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_box_integer", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as i64 {
+                black_box(f62::box_integer(black_box(index)));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_unbox_integer", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::unbox_integer(black_box(index)));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_box_f62", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::box_f62(black_box(f64::from_bits(index))));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_unbox_f62", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::unbox_f62(black_box(index)));
+            }
+        })
+    });
+}
+
+criterion_group!(benches, sum, f64, f62);
 
 criterion_main!(benches);
