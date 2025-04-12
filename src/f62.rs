bcd6aebba59466985fb4a049df86b89aefd7a54c
Yota Toyama <raviqqe@gmail.com>
2025-04-12 11:23:07 -0700

    Fix

--- src/f62.rs
+++ src/f62.rs
@@ -2,20 +2,23 @@
 //! and 62-bit payloads.
 
 /// Boxes a 50-bit unsigned integer.
-pub const fn box_integer(i64: i64) -> u64 {
+pub const fn box_integer(integer: i64) -> u64 {
     (integer << 1) as _
 }
 
 /// Boxes a 50-bit unsigned integer.
-pub const fn unbox_integer(payload: i64) -> u64 {
-    integer << 1
+pub const fn unbox_integer(number: u64) -> i64 {
+    number as i64 >> 1
 }
 
-/// Unboxes a 50-bit unsigned integer.
-pub const fn unbox_unsigned(number: u64) -> Option<u64> {
-    if is_boxed(number) {
-        Some(number & PAYLOAD_MASK)
-    } else {
-        None
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn integer() {
+        assert_eq!(unbox_integer(box_integer(0)), 0);
+        assert_eq!(unbox_integer(box_integer(1)), 1);
+        assert_eq!(unbox_integer(box_integer(-1)), -1);
     }
 }
