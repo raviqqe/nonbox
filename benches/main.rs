a6b526c33cd30fcd06d80a9e1bdc23c0903c485f
Yota Toyama <raviqqe@gmail.com>
2025-04-11 22:28:21 -0700

    Add benchmark

--- benches/main.rs
+++ benches/main.rs
@@ -1,12 +1,12 @@
 #![allow(missing_docs)]
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use nonbox::f64;
+use nonbox::{f64, n64::N64};
 
 const ITERATION_COUNT: usize = 10000;
 
 fn sum(criterion: &mut Criterion) {
-    criterion.bench_function("sum_pure", |bencher| {
+    criterion.bench_function("sum_native", |bencher| {
         bencher.iter(|| {
             let mut sum = 0.0f64;
 
@@ -18,12 +18,24 @@ fn sum(criterion: &mut Criterion) {
         })
     });
 
-    criterion.bench_function("sum", |bencher| {
+    criterion.bench_function("sum_f64", |bencher| {
         bencher.iter(|| {
             let mut sum = 0.0f64;
 
             for index in 0..black_box(ITERATION_COUNT as u64) {
                 sum += f64::from_bits(f64::box_unsigned(index));
+            }
+
+            black_box(sum);
+        })
+    });
+
+    criterion.bench_function("sum_n64", |bencher| {
+        bencher.iter(|| {
+            let mut sum = N64::from_signed_integer(0);
+
+            for index in 0..black_box(ITERATION_COUNT as u64) {
+                sum = sum + N64::from_signed_integer(index as _);
             }
 
             black_box(sum);
