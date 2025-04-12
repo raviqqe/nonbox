b64af5895243b808386e7c45a5e39ef2fdb59df6
Yota Toyama <raviqqe@gmail.com>
2025-04-12 14:58:27 +0900

    More benchmarks (#34)
    - **Add more benchmarks**
    - **Fix**
    - **Fix**
    - **Fix**

--- benches/main.rs
+++ benches/main.rs
@@ -6,24 +6,12 @@ use nonbox::f64;
 const ITERATION_COUNT: usize = 10000;
 
 fn sum(criterion: &mut Criterion) {
-    criterion.bench_function("sum_pure", |bencher| {
-        bencher.iter(|| {
-            let mut sum = 0.0f64;
-
-            for index in 0..black_box(ITERATION_COUNT as u64) {
-                sum += index as f64;
-            }
-
-            black_box(sum);
-        })
-    });
-
     criterion.bench_function("sum", |bencher| {
         bencher.iter(|| {
-            let mut sum = 0.0f64;
+            let mut sum = 0;
 
-            for index in 0..black_box(ITERATION_COUNT as u64) {
-                sum += f64::from_bits(f64::box_unsigned(index));
+            for index in 0..ITERATION_COUNT as u64 {
+                sum += black_box(index);
             }
 
             black_box(sum);
@@ -31,6 +19,26 @@ fn sum(criterion: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, sum);
+fn box_unsigned(criterion: &mut Criterion) {
+    criterion.bench_function("box_unsigned", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f64::box_unsigned(index));
+            }
+        })
+    });
+}
+
+fn box_signed(criterion: &mut Criterion) {
+    criterion.bench_function("box_signed", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as i64 {
+                black_box(f64::box_signed(index));
+            }
+        })
+    });
+}
+
+criterion_group!(benches, sum, box_unsigned, box_signed);
 
 criterion_main!(benches);
