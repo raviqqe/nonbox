c555d4e9b7db09b1ad1d6bc6261f61923013359a
Yota Toyama <raviqqe@gmail.com>
2025-04-12 11:34:13 -0700

    Fix

--- src/f62.rs
+++ src/f62.rs
@@ -7,8 +7,26 @@ pub const fn box_integer(integer: i64) -> u64 {
 }
 
 /// Boxes a 63-bit unsigned integer.
-pub const fn unbox_integer(number: u64) -> i64 {
-    number as i64 >> 1
+pub const fn unbox_integer(number: u64) -> Option<i64> {
+    if number & 1 == 0 {
+        Some(number as i64 >> 1)
+    } else {
+        None
+    }
+}
+
+/// Boxes a 62-bit payload.
+pub const fn box_payload(payload: u64) -> u64 {
+    (payload << 2) & 1
+}
+
+/// Boxes a 62-bit payload.
+pub const fn unbox_payload(number: u64) -> Option<u64> {
+    if number & 0b11 == 1 {
+        Some(number >> 2)
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -17,19 +35,19 @@ mod tests {
 
     #[test]
     fn integer() {
-        assert_eq!(unbox_integer(box_integer(0)), 0);
-        assert_eq!(unbox_integer(box_integer(1)), 1);
-        assert_eq!(unbox_integer(box_integer(-1)), -1);
-        assert_eq!(unbox_integer(box_integer(42)), 42);
-        assert_eq!(unbox_integer(box_integer(-42)), -42);
+        assert_eq!(unbox_integer(box_integer(0)), Some(0));
+        assert_eq!(unbox_integer(box_integer(1)), Some(1));
+        assert_eq!(unbox_integer(box_integer(-1)), Some(-1));
+        assert_eq!(unbox_integer(box_integer(42)), Some(42));
+        assert_eq!(unbox_integer(box_integer(-42)), Some(-42));
     }
 
     #[test]
     fn float() {
-        assert_eq!(unbox_integer(box_integer(0)), 0);
-        assert_eq!(unbox_integer(box_integer(1)), 1);
-        assert_eq!(unbox_integer(box_integer(-1)), -1);
-        assert_eq!(unbox_integer(box_integer(42)), 42);
-        assert_eq!(unbox_integer(box_integer(-42)), -42);
+        assert_eq!(unbox_integer(box_integer(0)), Some(0));
+        assert_eq!(unbox_integer(box_integer(1)), Some(1));
+        assert_eq!(unbox_integer(box_integer(-1)), Some(-1));
+        assert_eq!(unbox_integer(box_integer(42)), Some(42));
+        assert_eq!(unbox_integer(box_integer(-42)), Some(-42));
     }
 }
