eb66e38afb8884df7ad1615964571e7da99d91ec
Yota Toyama <raviqqe@gmail.com>
2025-04-12 11:24:05 -0700

    Fix

--- src/f62.rs
+++ src/f62.rs
@@ -1,12 +1,12 @@
 //! NaN boxing for 62-bit floating-pointer numbers encompassing 63-bit integers
 //! and 62-bit payloads.
 
-/// Boxes a 50-bit unsigned integer.
+/// Boxes a 63-bit unsigned integer.
 pub const fn box_integer(integer: i64) -> u64 {
     (integer << 1) as _
 }
 
-/// Boxes a 50-bit unsigned integer.
+/// Boxes a 63-bit unsigned integer.
 pub const fn unbox_integer(number: u64) -> i64 {
     number as i64 >> 1
 }
@@ -17,6 +17,15 @@ mod tests {
 
     #[test]
     fn integer() {
+        assert_eq!(unbox_integer(box_integer(0)), 0);
+        assert_eq!(unbox_integer(box_integer(1)), 1);
+        assert_eq!(unbox_integer(box_integer(-1)), -1);
+        assert_eq!(unbox_integer(box_integer(42)), 42);
+        assert_eq!(unbox_integer(box_integer(-42)), -42);
+    }
+
+    #[test]
+    fn float() {
         assert_eq!(unbox_integer(box_integer(0)), 0);
         assert_eq!(unbox_integer(box_integer(1)), 1);
         assert_eq!(unbox_integer(box_integer(-1)), -1);
