95c55fb391fa4528f661ad9414ba09f7cacb2d08
Yota Toyama <raviqqe@gmail.com>
2026-07-05 11:17:15 -0700

    More benchmarks

--- benches/main.rs
+++ benches/main.rs
@@ -8,6 +8,37 @@ use nonbox::{
 
 const ITERATION_COUNT: usize = 10000;
 
+fn binary<T>(
+    criterion: &mut Criterion,
+    name: &str,
+    left: &[Float62],
+    right: &[Float62],
+    operate: impl Fn(Float62, Float62) -> T,
+) {
+    criterion.bench_function(name, |bencher| {
+        bencher.iter(|| {
+            for (x, y) in left.iter().zip(right) {
+                black_box(operate(black_box(*x), black_box(*y)));
+            }
+        })
+    });
+}
+
+fn unary<T>(
+    criterion: &mut Criterion,
+    name: &str,
+    values: &[Float62],
+    operate: impl Fn(Float62) -> T,
+) {
+    criterion.bench_function(name, |bencher| {
+        bencher.iter(|| {
+            for x in values {
+                black_box(operate(black_box(*x)));
+            }
+        })
+    });
+}
+
 fn sum(criterion: &mut Criterion) {
     criterion.bench_function("sum_u64", |bencher| {
         let xs = (0..ITERATION_COUNT as i64).collect::<Vec<_>>();
@@ -88,6 +119,14 @@ fn f64(criterion: &mut Criterion) {
             }
         })
     });
+
+    criterion.bench_function("f64_is_boxed", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f64::is_boxed(black_box(index)));
+            }
+        })
+    });
 }
 
 fn f62(criterion: &mut Criterion) {
@@ -103,6 +142,14 @@ fn f62(criterion: &mut Criterion) {
         bencher.iter(|| {
             for index in 0..ITERATION_COUNT as u64 {
                 black_box(f62::unbox_payload(black_box(index)));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_unbox_payload_unchecked", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::unbox_payload_unchecked(black_box(index)));
             }
         })
     });
@@ -123,6 +170,14 @@ fn f62(criterion: &mut Criterion) {
         })
     });
 
+    criterion.bench_function("f62_unbox_integer_unchecked", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::unbox_integer_unchecked(black_box(index)));
+            }
+        })
+    });
+
     criterion.bench_function("f62_box_float", |bencher| {
         bencher.iter(|| {
             for index in 0..ITERATION_COUNT as u64 {
@@ -138,8 +193,175 @@ fn f62(criterion: &mut Criterion) {
             }
         })
     });
+
+    criterion.bench_function("f62_unbox_float_unchecked", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::unbox_float_unchecked(black_box(index)));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_is_integer", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::is_integer(black_box(index)));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_is_payload", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::is_payload(black_box(index)));
+            }
+        })
+    });
+
+    criterion.bench_function("f62_is_float", |bencher| {
+        bencher.iter(|| {
+            for index in 0..ITERATION_COUNT as u64 {
+                black_box(f62::is_float(black_box(index)));
+            }
+        })
+    });
 }
 
-criterion_group!(benches, sum, f64, f62);
+fn arithmetic(criterion: &mut Criterion) {
+    let integers = (0..ITERATION_COUNT as i64)
+        .map(Float62::from_integer)
+        .collect::<Vec<_>>();
+    let divisor_integers = (0..ITERATION_COUNT as i64)
+        .map(|x| Float62::from_integer(x % 256 + 1))
+        .collect::<Vec<_>>();
+    let floats = (0..ITERATION_COUNT)
+        .map(|x| Float62::from_float(x as f64 + 0.5))
+        .collect::<Vec<_>>();
+    let divisor_floats = (0..ITERATION_COUNT)
+        .map(|x| Float62::from_float((x % 256) as f64 + 1.5))
+        .collect::<Vec<_>>();
+    let overflowing_integers = (0..ITERATION_COUNT as i64)
+        .map(|x| Float62::from_integer((1 << 61) + x % 256))
+        .collect::<Vec<_>>();
+    let large_integers = (0..ITERATION_COUNT as i64)
+        .map(|x| Float62::from_integer((1 << 40) + x))
+        .collect::<Vec<_>>();
+
+    binary(
+        criterion,
+        "f62_add_integer",
+        &integers,
+        &integers,
+        |x, y| x + y,
+    );
+    binary(
+        criterion,
+        "f62_sub_integer",
+        &integers,
+        &integers,
+        |x, y| x - y,
+    );
+    binary(
+        criterion,
+        "f62_mul_integer",
+        &integers,
+        &integers,
+        |x, y| x * y,
+    );
+    binary(
+        criterion,
+        "f62_div_integer",
+        &integers,
+        &divisor_integers,
+        |x, y| x / y,
+    );
+    binary(
+        criterion,
+        "f62_rem_integer",
+        &integers,
+        &divisor_integers,
+        |x, y| x % y,
+    );
+    binary(
+        criterion,
+        "f62_checked_rem_integer",
+        &integers,
+        &divisor_integers,
+        |x, y| x.checked_rem(y),
+    );
+    unary(criterion, "f62_neg_integer", &integers, |x| -x);
+
+    binary(criterion, "f62_add_float", &floats, &floats, |x, y| x + y);
+    binary(criterion, "f62_sub_float", &floats, &floats, |x, y| x - y);
+    binary(criterion, "f62_mul_float", &floats, &floats, |x, y| x * y);
+    binary(
+        criterion,
+        "f62_div_float",
+        &floats,
+        &divisor_floats,
+        |x, y| x / y,
+    );
+    binary(
+        criterion,
+        "f62_rem_float",
+        &floats,
+        &divisor_floats,
+        |x, y| x % y,
+    );
+    unary(criterion, "f62_neg_float", &floats, |x| -x);
+
+    binary(criterion, "f62_add_mixed", &integers, &floats, |x, y| x + y);
+    binary(criterion, "f62_mul_mixed", &integers, &floats, |x, y| x * y);
+
+    binary(
+        criterion,
+        "f62_add_integer_overflow",
+        &overflowing_integers,
+        &overflowing_integers,
+        |x, y| x + y,
+    );
+    binary(
+        criterion,
+        "f62_mul_integer_overflow",
+        &large_integers,
+        &large_integers,
+        |x, y| x * y,
+    );
+}
+
+fn comparison(criterion: &mut Criterion) {
+    let integers = (0..ITERATION_COUNT as i64)
+        .map(Float62::from_integer)
+        .collect::<Vec<_>>();
+    let other_integers = (0..ITERATION_COUNT as i64)
+        .map(|x| Float62::from_integer(ITERATION_COUNT as i64 - x))
+        .collect::<Vec<_>>();
+    let floats = (0..ITERATION_COUNT)
+        .map(|x| Float62::from_float(x as f64 + 0.5))
+        .collect::<Vec<_>>();
+    let other_floats = (0..ITERATION_COUNT)
+        .map(|x| Float62::from_float((ITERATION_COUNT - x) as f64 + 0.5))
+        .collect::<Vec<_>>();
+
+    binary(
+        criterion,
+        "f62_cmp_integer",
+        &integers,
+        &other_integers,
+        |x, y| x.partial_cmp(&y),
+    );
+    binary(
+        criterion,
+        "f62_cmp_float",
+        &floats,
+        &other_floats,
+        |x, y| x.partial_cmp(&y),
+    );
+    binary(criterion, "f62_cmp_mixed", &integers, &floats, |x, y| {
+        x.partial_cmp(&y)
+    });
+}
+
+criterion_group!(benches, sum, f64, f62, arithmetic, comparison);
 
 criterion_main!(benches);
